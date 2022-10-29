0419cc25712533e081dedab15b6a986f0a28b033
Michal Orlik <sogehige@gmail.com>
2022-10-29 11:16:38 +0200

    feat: add working YT token generator without refreshing token

--- cloudflare/youtube-token-generator/src/index.js
+++ cloudflare/youtube-token-generator/src/index.js
@@ -14,6 +14,48 @@ import queryString from 'query-string';
 
 const router = Router()
 
+// https://oauth2.example.com/auth?code=4/P7q7W91a-oMsCeLvIaQm6bTrgtp7
+router.get("/", async ({query}) => {
+	if (query.code) {
+    try {
+			const params = new URLSearchParams();
+			params.append('code', query.code);
+			params.append('client_id', GOOGLE_CLIENT_ID);
+			params.append('client_secret', GOOGLE_CLIENT_SECRET);
+			params.append('redirect_uri', REDIRECT_URI);
+			params.append('grant_type', 'authorization_code');
+
+			console.log(JSON.stringify({
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				body: params.toString(),
+        method: 'POST',
+      }, null, 2))
+
+      const response = await fetch(`https://oauth2.googleapis.com/token`, {
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				body: params.toString(),
+        method: 'POST',
+      })
+      if (response.ok) {
+        const data = await response.json()
+        return new Response(`Access token: ${data.access_token}\nRefresh token: ${data.refresh_token}\nClient ID: ${GOOGLE_CLIENT_ID}`, {
+          headers: { 'content-type': 'text/plain' },
+        })
+      } else {
+  			return new Response("400, code already used!", { status: 400 })
+			}
+    } catch (e) {
+      return new Response("400, code incorrect!", { status: 400 })
+      console.error(e)
+    }
+  }
+  return new Response("404, not found!", { status: 404 })
+})
+
 router.get("/login", async (opts) => {
   const error = opts.query.error;
   const state = decodeURIComponent(opts.query.state || '');
@@ -29,8 +71,10 @@ router.get("/login", async (opts) => {
     response_type: "code",
     scope: scopes.join(' '),
     include_granted_scopes: "true",
+		access_type: 'offline',
     state,
-});
+		prompt: 'consent',
+	});
 
 
   if (error && error.trim().length > 0) {
