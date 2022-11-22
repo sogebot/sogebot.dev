a23ade13325b623b42c54a4e10ac01b48d7c0c26
Michal Orlik <sogehige@gmail.com>
2022-11-22 19:24:19 +0100

    chore: revert experimental edge support (heap OOM error)

--- ui.admin-mui/next.config.js
+++ ui.admin-mui/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  experimental:    { runtime: 'experimental-edge' },
   reactStrictMode: false,
   trailingSlash:   true,
   images:          { unoptimized: true },
