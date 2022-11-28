2dfc16ec6f860a61769f823a3fced95cf068a753
Michal Orlik <sogehige@gmail.com>
2022-11-28 17:51:13 +0100

    chore: try nodejs runtime

--- ui.admin-mui/next.config.js
+++ ui.admin-mui/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  experimental: {
+    runtime: 'nodejs'
+  },
   reactStrictMode: false,
   trailingSlash:   true,
   images:          { unoptimized: true },
