a84fdaaabb3eb4ee82bcacf29e485ad09a731db5
Michal Orlik <sogehige@gmail.com>
2023-02-02 01:07:38 +0100

    update

--- ui.admin-mui/src/components/Form/OverlayEdit.tsx
+++ ui.admin-mui/src/components/Form/OverlayEdit.tsx
@@ -1,7 +1,10 @@
+import { mdiResize, mdiTarget } from '@mdi/js';
+import Icon from '@mdi/react';
+import { SettingsTwoTone } from '@mui/icons-material';
 import { LoadingButton } from '@mui/lab';
 import {
   Avatar,
-  Box, Button, CircularProgress, DialogContent, Divider, Fade, Unstable_Grid2 as Grid, List, ListItemButton, ListItemText, Paper, Stack,
+  Box, Button, CircularProgress, DialogContent, Divider, Fade, Unstable_Grid2 as Grid, IconButton, List, ListItemButton, ListItemText, Paper, Stack,
 } from '@mui/material';
 import { Overlay } from '@sogebot/backend/dest/database/entity/overlay';
 import { validateOrReject } from 'class-validator';
@@ -14,6 +17,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { getSocket } from '../../helpers/socket';
 import { useValidator } from '../../hooks/useValidator';
 import { DimensionViewable, setZoomDimensionViewable } from '../Moveable/DimensionViewable';
+import { RemoveButton, setZoomRemoveButton } from '../Moveable/RemoveButton';
 
 const emptyItem: Partial<Overlay> = {
   canvas: {
@@ -24,11 +28,17 @@ const emptyItem: Partial<Overlay> = {
   items: [],
 };
 
-const generateColorFromString = (stringInput: string) => {
-  const stringUniqueHash = [...stringInput].reduce((acc, char) => {
-    return char.charCodeAt(0) + ((acc << 5) - acc);
-  }, 0);
-  return `hsl(${stringUniqueHash % 360}, 30%, 30%)`;
+const generateColorFromString = (str: string) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = '#';
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
 };
 
 export const OverlayEdit: React.FC = () => {
@@ -80,8 +90,8 @@ export const OverlayEdit: React.FC = () => {
     setBounds({
       left:   0,
       top:    0,
-      right:  item.canvas.width * zoom,
-      bottom: item.canvas.height * zoom,
+      right:  item.canvas.width,
+      bottom: item.canvas.height,
     });
   }, [moveableId, item, zoom]);
 
@@ -136,6 +146,7 @@ export const OverlayEdit: React.FC = () => {
       const zoomWidth = ((containerRef.current.getBoundingClientRect().width) / containerRef.current.scrollWidth);
       setZoom(Math.min(zoomHeight, zoomWidth));
       setZoomDimensionViewable(Math.min(zoomHeight, zoomWidth));
+      setZoomRemoveButton(Math.min(zoomHeight, zoomWidth));
     }
   }, [loading, containerRef.current]);
 
@@ -150,41 +161,59 @@ export const OverlayEdit: React.FC = () => {
       ><CircularProgress color="inherit" /></Grid>}
     <Fade in={!loading}>
       { item && <DialogContent>
-        {JSON.stringify({
-          frame, zoom, bounds, key,
-        })}
         <Grid container spacing={2} sx={{ height: 'calc(100% - 10px)' }}>
           <Grid xs={3}>
             <Box>
-              <List dense>
-                {item.items.map(o => <ListItemButton key={o.id} onClick={() => setMoveableId(o.id.replace(/-/g, ''))}>
-                  <ListItemText primary={<Stack direction='row' alignItems='center'>
+              <List dense sx={{ p: 0 }}>
+                {item.items.map(o => <ListItemButton
+                  key={o.id}
+                  selected={moveableId === o.id.replace(/-/g, '')}
+                  onClick={() => setMoveableId(o.id.replace(/-/g, ''))}>
+                  <ListItemText primary={<Stack direction='row' alignItems='center' sx={{
+                    textTransform: 'uppercase',
+                    '& small':     { fontSize: '8px !important' },
+                  }}>
                     <Avatar sx={{
                       backgroundColor: generateColorFromString(o.id),
                       width:           24,
                       height:          24,
-                      mr:              2,
+                      mr:              0.5,
                     }}>{' '}</Avatar>
-                    {o.name && o.name.length > 0 ? o.name : o.opts.typeId}
+                    <div>
+                      {o.name && o.name.length > 0
+                        ? <>
+                          {o.name} <small>{o.opts.typeId}</small>
+                        </>
+                        : o.opts.typeId}
+                    </div>
                   </Stack>}
-                  secondary={<>
-                    w: {o.width}px, h: {o.height}px, {o.alignX}x{o.alignY}
-                  </>}/>
+                  secondary={<small>
+                    <Icon path={mdiResize} size={'12px'} style={{
+                      marginRight: '2px', position: 'relative', top: '2px',
+                    }} />{o.width}x{o.height}
+                    <Icon path={mdiTarget} size={'14px'} style={{
+                      marginLeft: '5px', position: 'relative', top: '2px',
+                    }} />{o.alignX}x{o.alignY}
+                  </small>}/>
+                  <IconButton edge="end"><SettingsTwoTone/></IconButton>
                 </ListItemButton>,
                 )}
               </List>
             </Box>
           </Grid>
           <Grid xs>
-            <Box sx={{
+            <Box id="container" sx={{
               width: '100%', height: '100%', position: 'relative',
             }}  ref={containerRef}>
-              <Paper id="container"
+              <Paper
                 onClick={() => setMoveableId(null)}
                 sx={{
-                  height:   `${item.canvas.height * zoom}px`,
-                  width:    `${item.canvas.width * zoom}px`,
-                  position: 'absolute',
+                  height:                 `${item.canvas.height}px`,
+                  width:                  `${item.canvas.width}px`,
+                  position:               'absolute',
+                  transformOrigin:        '0 0',
+                  transform:              `scale(${zoom})`,
+                  '.moveable-size-value': { fontSize: `${14/zoom}px !important` },
                 }}>
                 {item.items.map(o => <Paper
                   id={o.id.replace(/-/g, '')}
@@ -195,26 +224,54 @@ export const OverlayEdit: React.FC = () => {
                     e.preventDefault();
                   }}
                   sx={{
-                    zIndex:          moveableId === o.id.replace(/-/g, '') ? '2': undefined,
-                    opacity:         moveableId === o.id.replace(/-/g, '') || moveableId == null ? '1': '0.2',
-                    position:        'absolute',
-                    width:           `${o.width * zoom}px`,
-                    height:          `${o.height * zoom}px`,
-                    backgroundColor: generateColorFromString(o.id),
-                    left:            `${o.alignX * zoom}px`,
-                    top:             `${o.alignY * zoom}px`,
+                    zIndex:           moveableId === o.id.replace(/-/g, '') ? '2': undefined,
+                    opacity:          moveableId === o.id.replace(/-/g, '') || moveableId == null ? '1': '0.2',
+                    position:         'absolute',
+                    width:            `${o.width}px`,
+                    height:           `${o.height}px`,
+                    backgroundColor:  generateColorFromString(o.id),
+                    left:             `${o.alignX}px`,
+                    top:              `${o.alignY}px`,
+                    fontWeight:       '900',
+                    fontSize:         `${20  / zoom}px`,
+                    textTransform:    'uppercase',
+                    textFillColor:    'white',
+                    WebkitTextStroke: `${1 / zoom}px black`,
+                    alignItems:       'center',
+                    justifyContent:   'center',
+                    textAlign:        'center',
+                    display:          'flex',
+                    userSelect:       'none',
+                    lineHeight:       `${12  / zoom}px`,
+                    '& small':        { fontSize: `${12  / zoom}px` },
                   }}
                 >
-                  {o.name && o.name.length > 0 ? o.name : o.opts.typeId}
+                  <div>
+                    {o.name && o.name.length > 0
+                      ? <>
+                        {o.name} <br/><small>{o.opts.typeId}</small>
+                      </>
+                      : o.opts.typeId}
+                  </div>
                 </Paper>)}
 
                 {moveableId && <Moveable
                   key={moveableId + key}
-                  ables={[DimensionViewable]}
-                  props={{ dimensionViewable: true }}
+                  ables={[DimensionViewable, RemoveButton]}
+                  props={{
+                    dimensionViewable: true, removeButton: true,
+                  }}
                   target={moveableRef}
                   resizable={true}
+                  origin={false}
                   bounds={bounds}
+                  onDelete={() => {
+                    setItem(o => ({
+                      ...o,
+                      items: o.items.filter(i => i.id.replace(/-/g, '') !== moveableId),
+                    }) as any);
+                    setMoveableId(null);
+                  }}
                   verticalGuidelines={[item.canvas.height / 4, item.canvas.height / 2, (item.canvas.height / 4) * 3]}
                   horizontalGuidelines={[item.canvas.width / 4, item.canvas.width / 2, (item.canvas.width / 4) * 3]}
                   elementGuidelines={elementGuidelines}
@@ -223,27 +280,25 @@ export const OverlayEdit: React.FC = () => {
                   isDisplaySnapDigit={true}
                   snapGap={true}
                   snapDirections={{
-                    'top': true,'right': true,'bottom': true,'left': true, 'center': true,
+                    'top': true,'right': true,'bottom': true,'left': true, 'center': true, middle: true,
                   }}
                   elementSnapDirections={{
-                    'top': true,'right': true,'bottom': true,'left': true, 'center': true,
+                    'top': true,'right': true,'bottom': true,'left': true, 'center': true, middle: true,
                   }}
                   snapDigit={0}
                   draggable={true}
                   throttleDrag={0}
                   startDragRotate={0}
                   throttleDragRotate={0}
-                  zoom={1}
                   padding={{
                     'left': 0,'top': 0,'right': 0,'bottom': 0,
                   }}
                   onResizeEnd={e => {
                     if (selectedItem) {
-                      console.log({ e });
-                      handleItemChange('width', Math.round((e.target as any).offsetWidth / zoom));
-                      handleItemChange('height', Math.round((e.target as any).offsetHeight / zoom));
-                      handleItemChange('alignX', Math.round(selectedItem.alignX + frame.translate[0] / zoom));
-                      handleItemChange('alignY', Math.round(selectedItem.alignY + frame.translate[1] / zoom));
+                      handleItemChange('width', Math.round((e.target as any).offsetWidth));
+                      handleItemChange('height', Math.round((e.target as any).offsetHeight));
+                      handleItemChange('alignX', Math.round(selectedItem.alignX + frame.translate[0]));
+                      handleItemChange('alignY', Math.round(selectedItem.alignY + frame.translate[1]));
                       e.target.style.transform = `translate(0px, 0px)`;
                       refresh();
                     }
@@ -262,8 +317,8 @@ export const OverlayEdit: React.FC = () => {
                   }}
                   onDragEnd={(e) => {
                     if (selectedItem) {
-                      handleItemChange('alignX', Math.round(selectedItem.alignX + frame.translate[0] / zoom));
-                      handleItemChange('alignY', Math.round(selectedItem.alignY + frame.translate[1] / zoom));
+                      handleItemChange('alignX', Math.round(selectedItem.alignX + frame.translate[0]));
+                      handleItemChange('alignY', Math.round(selectedItem.alignY + frame.translate[1]));
                       e.target.style.transform = `translate(0px, 0px)`;
                       refresh();
                     }
--- ui.admin-mui/src/components/Moveable/DimensionViewable.tsx
+++ ui.admin-mui/src/components/Moveable/DimensionViewable.tsx
@@ -18,18 +18,18 @@ export const DimensionViewable = {
     return <div key={'dimension-viewer'} className={'moveable-dimension'} style={{
       position:     'absolute',
       left:         `${rect.width / 2}px`,
-      top:          `${rect.height + 20}px`,
+      top:          `${rect.height + 10}px`,
       background:   '#4af',
       borderRadius: '2px',
       padding:      '2px 4px',
       color:        'white',
-      fontSize:     '13px',
+      fontSize:     `${13 / zoom}px`,
       whiteSpace:   'nowrap',
       fontWeight:   'bold',
       willChange:   'transform',
       transform:    'translate(-50%, 0px)',
     }}>
-      {Math.round(rect.offsetWidth / zoom)} x {Math.round(rect.offsetHeight / zoom)}
+      {Math.round(rect.offsetWidth)} x {Math.round(rect.offsetHeight)}
     </div>;
   },
 } as const;
