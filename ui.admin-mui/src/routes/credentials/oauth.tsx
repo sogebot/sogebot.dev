648a72a762b84adea4dce443dce98901e6c038d6
Michal Orlik <sogehige@gmail.com>
2023-05-13 14:39:15 +0200

    update

--- ui.admin-mui/src/routes/credentials/oauth.tsx
+++ ui.admin-mui/src/routes/credentials/oauth.tsx
@@ -62,7 +62,7 @@ const OAuth = () => {
         ...data, scopes: twitchValidation.data.scopes,
       });
 
-      axios.post('https://1ccf-213-155-246-230.ngrok-free.app/user', undefined, twitchHeaders).finally(() => {
+      axios.post('https://eventsub.sogebot.xyz/user', undefined, twitchHeaders).finally(() => {
         window.location.assign(`${gotoAfterLogin || state.referrer || state.url}`);
       });
     } catch (error) {
