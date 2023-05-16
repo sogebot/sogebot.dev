7a26555c620fe92f525299ac9d222eb212d71167
Michal Orlik <sogehige@gmail.com>
2023-05-16 23:29:14 +0200

    fix(webhooks): update timeout

--- services/webhooks/handler/handler.go
+++ services/webhooks/handler/handler.go
@@ -233,7 +233,7 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	perSecond := 3
-	timeout := time.Now().Add((time.Minute * 2) - 45*time.Second)
+	timeout := time.Now().Add((time.Minute * 2) - 15*time.Second)
 
 	go Listen(userId)
 	defer Done(userId)
