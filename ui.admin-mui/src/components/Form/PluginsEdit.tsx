2b407e8d48619eba0ef3290dce7565412ae86f3c
Michal Orlik <sogehige@gmail.com>
2023-08-12 22:43:47 +0200

    fix: update plugins

--- ui.admin-mui/src/components/Form/PluginsEdit.tsx
+++ ui.admin-mui/src/components/Form/PluginsEdit.tsx
@@ -37,7 +37,7 @@ import LODASH_string from '!raw-loader!@types/lodash/common/string.d.ts';
 import LODASH_util from '!raw-loader!@types/lodash/common/util.d.ts';
 // @ts-ignore
 import LODASH_index from '!raw-loader!@types/lodash/index.d.ts';
-import { ContentPasteTwoTone, DriveFileRenameOutlineTwoTone, LinkTwoTone, MenuTwoTone } from '@mui/icons-material';
+import { ContentPasteTwoTone, LinkTwoTone } from '@mui/icons-material';
 import { Plugin } from '@sogebot/backend/dest/database/entity/plugins';
 import shortid from 'shortid';
 import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
@@ -101,16 +101,15 @@ declare const Twitch: {
   /**
    * Bot will send message to chat
    * */
-  sendMessage(message:string, sendAs?: 'bot' | 'broadcaster'): void;
-  sendReply(messageId: string, message:string, sendAs?: 'bot' | 'broadcaster'): void;
-  timeout(userName: string, seconds: number): void;
+  sendMessage(message:string): void;
+  timeout(userId: string, seconds: number, reason?: string): void;
+  ban(userId: string, reason?: string): void;
 }
 declare const Permission: {
   /**
    * Check if user have access to this permissionId
    * */
   accessTo(userId: string, permissionId: string): Promise<boolean>;
-  list(): Promise<Permission[]>;
 }
 declare const Log: {
   info(text: string): void,
@@ -124,7 +123,6 @@ declare const permission = {
   MODERATORS:  'b38c5adb-e912-47e3-937a-89fabd12393a',
   SUBSCRIBERS: 'e3b557e7-c26a-433c-a183-e56c11003ab7',
   VIP:         'e8490e6e-81ea-400a-b93f-57f55aad8e31',
-  FOLLOWERS:   'c168a63b-aded-4a90-978f-ed357e95b0d2',
   VIEWERS:     '0efd7b1c-e460-4167-8e06-8aaf2c170311',
 } as const
 
