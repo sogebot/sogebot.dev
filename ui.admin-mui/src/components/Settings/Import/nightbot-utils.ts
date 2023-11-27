1d4f2493735a0f7fc1b1e595e509d04f38ff0813
redbeardy <redbeardymcgee@gmail.com>
2023-11-26 18:16:22 -0600

    refactor(import): mostly rearrangement

--- ui.admin-mui/src/components/Settings/Import/nightbot-utils.ts
+++ ui.admin-mui/src/components/Settings/Import/nightbot-utils.ts
@@ -5,6 +5,31 @@ import { enqueueSnackbar } from 'notistack';
 import getAccessToken from '../../../getAccessToken';
 import { getSocket } from '../../../helpers/socket';
 
+type Track = {
+  providerId: string;
+  provider:   string;
+  duration:   number;
+  title:      string;
+  artist:     string;
+  url:        string;
+};
+
+type PlaylistItem = {
+  track:     Track;
+  _id:       string;
+  createdAt: string; // timestamp date
+  updatedAt: string; // timestamp date
+};
+
+type PlaylistResponse = {
+  status:   number;
+  _sort:    { date: 'asc' | 'desc' };
+  _limit:   number;
+  _offset:  number;
+  _total:   number;
+  playlist: PlaylistItem[];
+};
+
 type UserLevel =
   | 'admin'
   | 'owner'
@@ -31,6 +56,16 @@ type CustomCommandsResponse = {
   commands: CustomCommand[];
 };
 
+const permissions = new Map([
+  ['admin', defaultPermissions.CASTERS],
+  ['owner', defaultPermissions.CASTERS],
+  ['moderator', defaultPermissions.MODERATORS],
+  ['twitch_vip', defaultPermissions.VIP],
+  ['regular', defaultPermissions.SUBSCRIBERS],
+  ['subscriber', defaultPermissions.SUBSCRIBERS],
+  ['everyone', defaultPermissions.VIEWERS],
+]);
+
 export const sleep = async (ms: number) => {
   await new Promise((resolve) => setTimeout(resolve, ms));
 };
@@ -51,152 +86,6 @@ const fetchWithRetries = async (
       await sleep(delay);
     }
   }
-};
-
-const fetchCustomCommandsPage = async (
-  accessToken: string | null
-): Promise<CustomCommandsResponse> => {
-  const url = 'https://api.nightbot.tv/1/commands';
-  try {
-    const page = fetchWithRetries(url, {
-      headers: { Authorization: 'Bearer ' + accessToken },
-    });
-    return page;
-  } catch {
-    console.error('Error fetching commands after multiple retries.');
-    enqueueSnackbar('Remote server error.', { variant: 'error' });
-    throw new Error('Failed to fetch commands after multiple retries.');
-  }
-};
-
-export const fetchCustomCommands = async (
-  accessToken: string | null
-): Promise<CustomCommand[]> => {
-  try {
-    const page = await fetchCustomCommandsPage(accessToken);
-    return page.commands;
-  } catch (error: any) {
-    console.error('Error fetching commands.');
-    enqueueSnackbar('Remote server error.', { variant: 'error' });
-    throw new Error('Failed to fetch commands.');
-  }
-};
-
-const permissionsMap = new Map([
-  ['admin', defaultPermissions.CASTERS],
-  ['owner', defaultPermissions.CASTERS],
-  ['moderator', defaultPermissions.MODERATORS],
-  ['twitch_vip', defaultPermissions.VIP],
-  ['regular', defaultPermissions.SUBSCRIBERS],
-  ['subscriber', defaultPermissions.SUBSCRIBERS],
-  ['everyone', defaultPermissions.VIEWERS],
-]);
-
-const postKeyword = async (command: CustomCommand) => {
-  const convertedKeyword = {
-    keyword:   command.name,
-    enabled:   true,
-    group:     'nightbot-import',
-    responses: [
-      {
-        order:          0,
-        response:       command.message,
-        stopIfExecuted: false,
-        filter:         '',
-        permission:     permissionsMap.get(command.userLevel),
-      },
-    ],
-  };
-  let failCount = 0;
-  try {
-    await axios.post(
-      `${JSON.parse(localStorage.server)}/api/systems/keywords`,
-      convertedKeyword,
-      { headers: { authorization: `Bearer ${getAccessToken()}` } }
-    );
-  } catch (error: any) {
-    failCount += 1;
-    console.error(
-      'ERROR DURING KEYWORDS IMPORT: ',
-      error.response.data.errors
-    );
-  }
-  if (failCount > 0) {
-    enqueueSnackbar(`${failCount} commands failed to import.`, {
-      variant: 'info',
-    });
-  }
-};
-
-const postCommand = async (command: CustomCommand) => {
-  const convertedCommand = {
-    command:                command.name,
-    enabled:                true,
-    visible:                true,
-    group:                  'nightbot-import',
-    areResponsesRandomized: false,
-    responses:              [
-      {
-        order:          0,
-        response:       command.message,
-        stopIfExecuted: false,
-        permission:     permissionsMap.get(command.userLevel),
-        filter:         '',
-      },
-    ],
-  };
-  let failCount = 0;
-  try {
-    await axios.post(
-      `${JSON.parse(localStorage.server)}/api/systems/customcommands`,
-      convertedCommand,
-      { headers: { authorization: `Bearer ${getAccessToken()}` } }
-    );
-  } catch (error: any) {
-    failCount += 1;
-    console.error(
-      'ERROR DURING COMMANDS IMPORT: ',
-      error.response.data.errors
-    );
-  }
-  if (failCount > 0) {
-    enqueueSnackbar(`${failCount} commands failed to import.`, {
-      variant: 'info',
-    });
-  }
-};
-
-export const importCustomCommands = async (accessToken: string | null) => {
-  const commands = await fetchCustomCommands(accessToken);
-  commands.forEach((command) =>
-    command.name.startsWith('!') ? postCommand(command) : postKeyword(command)
-  );
-  enqueueSnackbar('Commands import completed.', { variant: 'success' });
-};
-
-type Track = {
-  providerId: string;
-  provider:   string;
-  duration:   number;
-  title:      string;
-  artist:     string;
-  url:        string;
-};
-
-type PlaylistItem = {
-  track:     Track;
-  _id:       string;
-  createdAt: string; // timestamp date
-  updatedAt: string; // timestamp date
-};
-
-type PlaylistResponse = {
-  status:   number;
-  _sort:    { date: 'asc' | 'desc' };
-  _limit:   number;
-  _offset:  number;
-  _total:   number;
-  playlist: PlaylistItem[];
 };
 
 const fetchPlaylistPage = async (
@@ -228,15 +117,33 @@ const fetchTracks = async (
   try {
     const page = await fetchPlaylistPage(offset, accessToken);
     const mergedTracks = tracks.concat(page.playlist.map((t) => t.track));
-    if (mergedTracks.length < page._total) {
-      await fetchTracks(accessToken, mergedTracks, offset + 100);
-    }
-    return mergedTracks;
-  } catch (error: any) {
+    return mergedTracks.length < page._total
+      ? await fetchTracks(accessToken, mergedTracks, offset + 100)
+      : mergedTracks;
+  } catch {
     console.error('Error fetching playlist.');
     enqueueSnackbar('Remote server error.', { variant: 'error' });
     throw new Error('Failed to fetch playlist.');
   }
+};
+
+const postTrack = async (track: Track) => {
+  await new Promise((resolve, reject) => {
+    getSocket('/systems/songs').emit(
+      'import.video',
+      {
+        playlist:  track.providerId,
+        forcedTag: 'nightbot-import',
+      },
+      (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve('resolved');
+        }
+      }
+    );
+  });
 };
 
 export const importPlaylist = async (accessToken: string | null) => {
@@ -245,26 +152,10 @@ export const importPlaylist = async (accessToken: string | null) => {
   let failCount = 0;
   for (const track of ytVideos) {
     try {
-      await new Promise((resolve, reject) => {
-        getSocket('/systems/songs').emit(
-          'import.video',
-          {
-            playlist:  track.providerId,
-            forcedTag: 'nightbot-import',
-          },
-          (err) => {
-            if (err) {
-              failCount += 1;
-              console.error('error: ', track.url);
-              reject(err);
-            } else {
-              resolve('resolved');
-            }
-          }
-        );
-      });
-    } catch (error) {
-      console.error('ERROR DURING IMPORT: ', error);
+      await postTrack(track);
+    } catch {
+      console.error('error: ', track.url);
+      failCount++;
     }
   }
   if (failCount > 0) {
@@ -273,4 +164,114 @@ export const importPlaylist = async (accessToken: string | null) => {
     });
   }
   enqueueSnackbar('Playlist import completed.', { variant: 'success' });
+};
+
+const fetchCustomCommandsPage = async (
+  accessToken: string | null
+): Promise<CustomCommandsResponse> => {
+  const url = 'https://api.nightbot.tv/1/commands';
+  try {
+    const page = fetchWithRetries(url, {
+      headers: { Authorization: 'Bearer ' + accessToken },
+    });
+    return page;
+  } catch {
+    console.error('Error fetching commands after multiple retries.');
+    enqueueSnackbar('Remote server error.', { variant: 'error' });
+    throw new Error('Failed to fetch commands after multiple retries.');
+  }
+};
+
+export const fetchCustomCommands = async (
+  accessToken: string | null
+): Promise<CustomCommand[]> => {
+  try {
+    const page = await fetchCustomCommandsPage(accessToken);
+    return page.commands;
+  } catch (error: any) {
+    console.error('Error fetching commands.');
+    enqueueSnackbar('Remote server error.', { variant: 'error' });
+    throw new Error('Failed to fetch commands.');
+  }
+};
+
+const postKeyword = async (keyword: CustomCommand) => {
+  const convertedKeyword = {
+    keyword:   keyword.name,
+    enabled:   true,
+    group:     'nightbot-import',
+    responses: [
+      {
+        order:          0,
+        response:       keyword.message,
+        stopIfExecuted: false,
+        filter:         '',
+        permission:     permissions.get(keyword.userLevel),
+      },
+    ],
+  };
+  try {
+    await axios.post(
+      `${JSON.parse(localStorage.server)}/api/systems/keywords`,
+      convertedKeyword,
+      { headers: { authorization: `Bearer ${getAccessToken()}` } }
+    );
+  } catch (error: any) {
+    console.error(
+      'ERROR DURING KEYWORDS IMPORT: ',
+      error.response.data.errors
+    );
+  }
+};
+
+const postCommand = async (command: CustomCommand) => {
+  const convertedCommand = {
+    command:                command.name,
+    enabled:                true,
+    visible:                true,
+    group:                  'nightbot-import',
+    areResponsesRandomized: false,
+    responses:              [
+      {
+        order:          0,
+        response:       command.message,
+        stopIfExecuted: false,
+        permission:     permissions.get(command.userLevel),
+        filter:         '',
+      },
+    ],
+  };
+  try {
+    await axios.post(
+      `${JSON.parse(localStorage.server)}/api/systems/customcommands`,
+      convertedCommand,
+      { headers: { authorization: `Bearer ${getAccessToken()}` } }
+    );
+  } catch (error: any) {
+    console.error(
+      'ERROR DURING COMMANDS IMPORT: ',
+      error.response.data.errors
+    );
+  }
+};
+
+export const importCustomCommands = async (accessToken: string | null) => {
+  const commands = await fetchCustomCommands(accessToken);
+  let failCount = 0;
+  for (const command of commands) {
+    try {
+      command.name.startsWith('!')
+        ? postCommand(command)
+        : postKeyword(command);
+    } catch (error: any) {
+      failCount++;
+    }
+  }
+  if (failCount > 0) {
+    enqueueSnackbar(`${failCount} commands failed to import.`, {
+      variant: 'info',
+    });
+  }
+  enqueueSnackbar('Review imported commands.', { variant: 'info' });
+  enqueueSnackbar('Commands import completed.', { variant: 'success' });
 };
