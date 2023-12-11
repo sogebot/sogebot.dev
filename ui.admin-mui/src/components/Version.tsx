82404a09c77af31bcfdc8935eea58e635accfd02
Michal Orlik <sogehige@gmail.com>
2023-12-11 21:18:10 +0100

    feat(version): add dashboard version check

--- ui.admin-mui/src/components/Version.tsx
+++ ui.admin-mui/src/components/Version.tsx
@@ -1,4 +1,6 @@
 import { Box, Fade, Typography } from '@mui/material';
+import axios from 'axios';
+import { useSnackbar } from 'notistack';
 import React from 'react';
 
 import { useAppSelector } from '../hooks/useAppDispatch';
@@ -8,7 +10,38 @@ console.log(process.env.REACT_APP_VERSION);
 console.groupEnd();
 
 export const Version: React.FC = () => {
-  const { connectedToServer } = useAppSelector(s => s.loader);
+  const { connectedToServer, server } = useAppSelector(s => s.loader);
+  const { enqueueSnackbar } = useSnackbar();
+
+  React.useEffect(() => {
+    if ((process.env.REACT_APP_COMMIT || '').length > 0) {
+      // we have base path, skip checks
+      return;
+    }
+
+    if (connectedToServer && server) {
+      axios.get(`${server}/_dash_version`)
+        .then(res => {
+          console.group('DOCKER UI VERSION');
+          console.log(res.data);
+          console.groupEnd();
+          if (process.env.REACT_APP_VERSION) {
+            if (!process.env.REACT_APP_VERSION.includes(res.data)) {
+              console.warn(`UI version (${process.env.REACT_APP_VERSION}) does not match docker version (${res.data})`);
+              enqueueSnackbar(<>There is new version of UI available. Please force refresh page by <Typography component='strong' variant='button' sx={{ fontWeight: 'bold', pl: 0.5, }}>Ctrl+Shift+R</Typography></>, {
+                variant: 'info',
+                persist: true,
+              });
+            } else {
+              console.log('UI version matches docker version');
+            }
+          }
+        })
+        .catch(err => {
+          console.error('Error getting server version:', err);
+        });
+    }
+  }, [connectedToServer, server]);
 
   return <Fade in={!connectedToServer} unmountOnExit mountOnEnter>
     <Box sx={{
