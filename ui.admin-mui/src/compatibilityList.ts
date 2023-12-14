b9290213f2a534a0e21d5cf76e0b5d7577dd4903
Michal Orlik <sogehige@gmail.com>
2023-12-14 11:08:55 +0100

    chore: test 3fbbd8a2 as compatibility

--- ui.admin-mui/src/compatibilityList.ts
+++ ui.admin-mui/src/compatibilityList.ts
@@ -8,6 +8,7 @@ export const versions: {
   '~19.3.0 || ~19.4.0 || ~20.0.0': '0509e1d6',
   '~19.2.0':                       '5b6ec5c5',
   '~19.1.0':                       'c52dfbce',
+  '18.0.0':                        '3fbbd8a2',
 } as const;
 
 console.debug('Current compatibility list:', versions);
