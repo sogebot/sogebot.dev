8c3fec92c9ddaf948d393bf326243ea8b084beb0
Michal Orlik <sogehige@gmail.com>
2025-03-25 11:52:54 +0100

    chore: potential build fix

--- ui.admin-mui/craco.config.js
+++ ui.admin-mui/craco.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = {
   webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.symlinks = false; // treat workspace packages as local
+      return webpackConfig;
+    },
     alias: {
       '@entity': path.resolve(__dirname, '../node_modules/@sogebot/backend/dest/database/entity'),
       '@backend': path.resolve(__dirname, '../node_modules/@sogebot/backend/dest'),
